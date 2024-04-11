076e5331416d58cb45f905fa444d0df736c0a87b
athariandre <athariandre@gmail.com>
2024-04-10 19:02:02 -0500

    added files

---
+++ Card.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+#include "Card.h"
+
+using std::string;
+
+Card::Card(string rank, string suit) /* TODO: initialize */ {
+  // TODO: implement constructor checks
+}
+
+string Card::getRank() {
+  // TODO: implement getter
+}
+
+string Card::getSuit() {
+  // TODO: implement getter
+}
+
+int Card::getTimesPlayed() {
+  // TODO: implement getter
+}
+
+bool Card::canBePlayed(string currentRank, string currentSuit) {
+  // TODO: return whether or not the card can legally be played
+}
+
+void Card::play() {
+  timesPlayed++;
+}
