caf38b3bb8a0328597cb3dda4cac41d076dbbe5e
athariandre <athariandre@gmail.com>
2024-04-15 03:08:59 -0500

    maybe

--- Player.cpp
+++ Player.cpp
@@ -70,16 +70,18 @@ Card* Player::playCard(vector<string> const& suits, string& currentRank, string&
           if(hand[i]->canBePlayed(currentRank, currentSuit)){   //if card exists and can be played
             if(hand[i]->getRank() == "8"){ //if card is eight, pick new suit first
               cout << "What suit would you like to declare?" << endl;
-              bool suitUnchanged = true;
-              while(suitUnchanged){
+              bool suitChanged = false;
+              while(!suitChanged){
                 cin >> newSuit;
                 for(int i = 0; i < suits.size(); i++){
                   if(suits[i] == newSuit){
-                    suitUnchanged = false;
+                    suitChanged = true;
                     break;
                   }
                 }
-                cout << "That's not a suit in this deck. Try again." << endl;
+                if(!suitChanged){
+                  cout << "That's not a suit in this deck. Try again." << endl;
+                }
               }
             }
             hand[i]->play();
